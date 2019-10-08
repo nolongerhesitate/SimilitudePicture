9defd7290d3213be4d605890b99eb6f8b5de0fb6
nolongerhesitate <nolongerhesitate@users.noreply.github.com>
2019-10-08 18:13:43 +0800

    可以通过文件查找文件夹下相似的图片

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDir>
+#include <QFileDialog>
 #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -8,6 +10,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->progressBar->setVisible(false);
+    ui->progressBar->setMinimum(0); // 最小值
 }
 
 MainWindow::~MainWindow()
@@ -17,19 +21,60 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QImage img1;
-    img1.load("C:\\Users\\Administrator\\Desktop\\49.png");
-    QImage img2;
-    img2.load("C:\\Users\\Administrator\\Desktop\\21.png");
+    if(imgPath.isEmpty() || imgPath.isNull()){
+        QMessageBox::warning(this,"错误","先选择图片");
+        return;
+    }
+    if(dirPath.isEmpty() || dirPath.isNull()){
+        QMessageBox::warning(this,"错误","先选择文件夹");
+        return;
+    }
 
+    ui->selImgBtn->setEnabled(false);
+    ui->selDirBtn->setEnabled(false);
+    ui->imgLineEdit->setEnabled(false);
+    ui->dirLineEdit->setEnabled(false);
+    ui->textEdit->setEnabled(false);
+    ui->pushButton->setEnabled(false);
+
+    ui->progressBar->setVisible(true);
+    /*
+    qDebug()<<imgPath<<endl;
+    qDebug()<<dirPath<<endl;
+    */
+    QImage img;
+    img.load(imgPath);
+
+    QDir dir(dirPath);
+    dir.setFilter(QDir::Files);//除了文件，其他的过滤掉
+    QFileInfoList list = dir.entryInfoList();//获取文件信息列表
+    ui->progressBar->setMaximum(list.size());  // 最大值
+    for(int i = 0; i< list.size();i++){
+        ui->progressBar->setValue(i);
+        if(!isPicture(list[i].filePath())) continue;
+        QImage tmpImg;
+        tmpImg.load(list[i].filePath());
+        if(isSimilitude(img,tmpImg,10)){
+            ui->textEdit->append(list[i].filePath());
+        }
+    }
+
+    ui->progressBar->setValue(list.size());
+    ui->selImgBtn->setEnabled(true);
+    ui->selDirBtn->setEnabled(true);
+    ui->imgLineEdit->setEnabled(true);
+    ui->dirLineEdit->setEnabled(true);
+    ui->textEdit->setEnabled(true);
+    ui->pushButton->setEnabled(true);
+    /*
     if(isSimilitude(img1,img2,10)){
         QMessageBox::information(this,"相似","两张图片相似");
     }else
     {
         QMessageBox::information(this,"不同","两张图片不相同");
     }
+    */
 }
-
 
 
 QImage MainWindow::toGray( QImage image )
@@ -76,11 +121,10 @@ double MainWindow::g_avg_pixel(QImage image)
     int height = image.height();
     int width = image.width();
 
-    for(int row=0;row<height; row+=1){
-        for (int cols =0;cols<width;cols+=1) {
-           QRgb rgb = image.pixel(row,cols);
-           qDebug()<<"rgb === "<<rgb<<endl;
-           avg_pixel += rgb;
+    for(int row=0;row<height; row++){
+        for (int cols =0;cols<width;cols++) {
+            QRgb rgb = image.pixel(cols,row);
+            avg_pixel += rgb;
         }
     }
     return avg_pixel/(height*width);
@@ -91,10 +135,10 @@ QString MainWindow::toHash(QImage image, const double& avg_pixel)
     QString hash = "";
     int height = image.height();
     int width = image.width();
-    for(int row=0;row<height; row+=1){
-        for (int cols =0;cols<width;cols+=1) {
-           QRgb rgb = image.pixel(row,cols);
-           hash += rgb>=avg_pixel?"1":"0";
+    for(int row=0;row<height; row++){
+        for (int cols =0;cols<width;cols++) {
+            QRgb rgb = image.pixel(cols,row);
+            hash += rgb>=avg_pixel?"1":"0";
         }
     }
     return hash;
@@ -126,13 +170,50 @@ bool MainWindow::isSimilitude(QImage img1, QImage img2, int weight)
     bool success = res.save("C:\\Users\\Administrator\\Desktop\\s.png","PNG");
     */
 
-    QImage res1 = toGray(img1.scaled(800,800,Qt::KeepAspectRatio,Qt::FastTransformation).scaled(10,10,Qt::KeepAspectRatio,Qt::SmoothTransformation));
+    QImage res1 = toGray(img1/*.scaled(800,800,Qt::KeepAspectRatio,Qt::FastTransformation)*/.scaled(10,10,Qt::KeepAspectRatio,Qt::SmoothTransformation));
     double avg1 = g_avg_pixel(res1);
     QString hash1 = toHash(res1,avg1);
-    QImage res2 = toGray(img2.scaled(800,800,Qt::KeepAspectRatio,Qt::FastTransformation).scaled(10,10,Qt::KeepAspectRatio,Qt::SmoothTransformation));
+    QImage res2 = toGray(img2/*.scaled(800,800,Qt::KeepAspectRatio,Qt::FastTransformation)*/.scaled(10,10,Qt::KeepAspectRatio,Qt::SmoothTransformation));
     double avg2 = g_avg_pixel(res2);
     QString hash2 = toHash(res2,avg2);
     return !(getHamming(hash1,hash2) > weight);
-//    bool success = res.save("C:\\Users\\Administrator\\Desktop\\s.png","PNG");
+    //    bool success = res.save("C:\\Users\\Administrator\\Desktop\\s.png","PNG");
 }
 
+bool MainWindow::isPicture(const QString &filePath)
+{
+    QPixmap pixs;
+    pixs.load(filePath);
+    return !(pixs.isNull());
+}
+
+
+void MainWindow::on_selImgBtn_clicked()
+{
+    //定义文件对话框类
+    QFileDialog *fileDialog = new QFileDialog(this);
+    //定义文件对话框标题
+    fileDialog->setWindowTitle(tr("打开图片"));
+    //设置默认文件路径
+//    fileDialog->setDirectory(".");
+    //设置文件过滤器
+    //    fileDialog->setNameFilter(tr("Images(*.png *.jpg *.jpeg *.bmp)"));
+    //设置视图模式
+    fileDialog->setViewMode(QFileDialog::Detail);
+    if(fileDialog->exec())
+    {
+        if(!isPicture(fileDialog->selectedFiles()[0])) {
+            QMessageBox::warning(this,"错误","请选择图片!");
+            return;
+        }
+        imgPath = fileDialog->selectedFiles()[0];
+        ui->imgLineEdit->setText(imgPath);
+    }
+
+}
+
+void MainWindow::on_selDirBtn_clicked()
+{
+    dirPath =  QFileDialog::getExistingDirectory (this, tr("Open Directory"), "/home", QFileDialog::ShowDirsOnly );
+    ui->dirLineEdit->setText(dirPath);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,10 +3,12 @@
 
 #include <QMainWindow>
 #include <QDebug>
+#include <QList>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
 
 class MainWindow : public QMainWindow
 {
@@ -20,11 +22,18 @@ public:
     QString toHash(QImage image, const double& avg_pixel); //获取hash值
     int getHamming(const QString& str1, const QString& str2); //计算两个字符串的汉明距离
     bool isSimilitude(QImage img1, QImage img2, int weight); // 是否相似, 第三个参数权重，当汉明距离超过这个数返回false
+    bool isPicture(const QString& filePath); // 是否为图片文件
 
 private slots:
     void on_pushButton_clicked();
 
+    void on_selImgBtn_clicked();
+
+    void on_selDirBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString imgPath;
+    QString dirPath;
 };
 #endif // MAINWINDOW_H
